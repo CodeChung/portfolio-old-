32f5165a2a80a5682191896e792ba5a98a2dc4b4
Harry Chung <hc9825@gmail.com>
2019-05-14 21:35:08 -0700

    implement navbar scroll highlighting, trying to add scroll snap

--- index.js
+++ index.js
@@ -19,5 +19,48 @@ function slideMain() {
 	});
 }
 
+// function scrollMainDown() {
+// 	if (findCurrentView().attr("href") !== "contact") {
+
+// 	}
+// }
+
+// check if element is in viewport
+$.fn.isInViewport = function() {
+	let elementTop = $(this).offset().top;
+	let elementBottom = elementTop + $(this).height();
+	let viewportTop = $(window).scrollTop();
+	let viewportBottom = viewportTop + $('section').height();
+	return Math.abs(elementTop-viewportTop) + Math.abs(elementBottom-viewportBottom);
+}
+
+function findCurrentView() {
+	let min = 100000;
+	let currentSection = null;
+	let sections = $('section');
+	sections.each(function() {
+		if ($(this).isInViewport() < min) {
+			min = $(this).isInViewport();
+			currentSection = $(this);
+		}
+	});
+	console.log(currentSection.attr("id"))
+	return currentSection;
+}
+
+function setCurrentNav() {
+	$(window).on('resize scroll', function() {
+		$('header ul li').each(function() {
+			if ($(this).children("a").attr("href") === "#" + findCurrentView().attr("id")) {
+				$(this).addClass('current')
+			} else {
+				$(this).removeClass("current")
+			}
+		});
+	})
+}
+
 $(navbarAnimate);
 $(slideMain);
+$(setCurrentNav);
+$(findCurrentView);
