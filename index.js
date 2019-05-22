dede8005469c8ed9eda4b35dd256b180193cce91
Harry Chung <hc9825@gmail.com>
2019-05-22 08:48:22 -0700

    navbar query change

--- index.js
+++ index.js
@@ -90,11 +90,25 @@ function scrollSnap(currentSection, direction) {
 	let position = $(sectionToSlide).offset().top;
 	slideMain(position);
 };
-function scrollScreen() {
-	$.scrollify({
-	  section : "section",
-	});
-}
+
+let items = $(".scroll-liner");
+let animating = false;
+
+$(window).scroll(function() {
+    clearTimeout($.data(this, 'scrollTimer'));
+    if (!animating) {
+        $.data(this, 'scrollTimer', setTimeout(function() {
+            items.each(function(key, value) {
+                if ($(value).offset().top >= $(window).scrollTop()) {
+                    animating = true;
+                    $('body').animate( { scrollTop: $(value).offset().top + 'px' }, 250);
+                    setTimeout(function() { animating = false; }, 300);
+                    return false;
+                }
+            });
+        }, 200));
+    }
+});
 
 $(navbarAnimate);
 $(slideOnClick);
