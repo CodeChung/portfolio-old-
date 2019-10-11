a61d85508ee43e3c4ec8ffaa330385bec1e0cc89
Harry Chung <hc9825@gmail.com>
2019-10-10 23:05:18 -0700

    title change

--- src/App.js
+++ src/App.js
@@ -57,6 +57,7 @@ class App extends React.Component {
         <main role="contentinfo">
           <Switch>
             <Route exact path='/' component={Intro} />
+            <Route path='/portfolio' component={Intro} />
             <Route path='/projects' component={Projects} />
             <Route path='/about' component={About} />
             <Route path='/contact' component={Contact} />
